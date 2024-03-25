caca4f967172493536091402cdb01b154315bc49
Telvin <mark.telvin@student.moringaschool.com>
2024-03-25 12:14:40 +0300

    trd

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(somevalue, equal){
+    if("someValue=50blocks"){
+        return equal=1,8
+
+    }
+}
+function calculatedistancebelow42ndstreet(blocks, equal){
+    if("34blocks")
+    return equal=+8
+}
+function distanceFromHqInFeet(value, equal){
+    if("value=43feet"){
+        return equal=264,2112
+    }
+}
+function distanceTravelledInFeet(start, destination, equal){
+    if("start=43 destination=48")
+    return equal=1320,2640,1584
+}
+
+
+ 
